008950418fc3cb442d60eeafd525c7b9fcbafa3f
cLubber1337 <supersoroko@gmail.com>
2022-10-28 00:45:48 +0300

    Task №1. String repeat

---
+++ codewars.js
@@ -0,0 +1,13 @@
+// Task №1. String repeat
+/*
+function repeatStr(n, s) {
+  let repeatedString = "";
+  while (n > 0) {
+    repeatedString += s;
+    n--;
+  }
+  return repeatedString;
+}
+console.log(repeatStr(5, "ha"));
+*/
+
