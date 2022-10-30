edb37e474d73a3e062b6691f3305c6d3af156df9
cLubber1337 <supersoroko@gmail.com>
2022-10-31 01:35:05 +0300

    Task №2. Grasshopper - Personalized Message

--- codewars.js
+++ codewars.js
@@ -9,5 +9,33 @@ function repeatStr(n, s) {
   return repeatedString;
 }
 console.log(repeatStr(5, "ha"));
+
+
+function repeatStr(n, s) {
+  return new Array(n + 1).join(s);
+}
+console.log(repeatStr(5, "L"));
+*/
+/*
+function repeatStr(n, s) {
+  return s.repeat(n);
+}
+console.log(repeatStr(5, "S"));
 */
 
+// Task №2. Grasshopper - Personalized Message
+/*
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+Use conditionals to return the proper message:
+case	return
+name equals owner	'Hello boss'
+otherwise	'Hello guest'
+*/
+/*
+function  (name, owner) {
+  if (name === owner) {
+    return "Hello boss"
+  }
+    return "Hello guest"
+}
+*/
